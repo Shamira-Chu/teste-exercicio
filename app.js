9b026c39dd52bb430958ab532d9f0752e97459ec
Shamira <shamiraxp10@gmail.com>
2025-09-04 11:20:53 -0300

    priint teste

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
+
+
 button.addEventListener('click', function() {
     //assincrona(Async Chaining)
   fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(function(response) {
 return response.json();
   })
-  
+
   .then(function(json) {
-console.log(json);
+div.textContent= json;
   })
 
 });

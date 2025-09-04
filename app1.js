79c8b82cadac97c493004aeebbfaa0b725feeb7c
Shamira <shamiraxp10@gmail.com>
2025-09-04 10:49:35 -0300

    btn-js

---
+++ app1.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('btnUsers');
+button.addEventListener('click', function() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(json => console.log(json));
+    //console.log('Clicou');
+});

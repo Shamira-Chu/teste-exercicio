d0070ac109c55dfd07b90d3c1c70de6eb3d6b8aa
Shamira <shamiraxp10@gmail.com>
2025-09-04 11:30:09 -0300

    tirar o /1

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ let button = document.getElementById('btnUsers');
 let div = document.getElementById('out');
 
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     //assincrona(Async Chaining)
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(function(response) {
-return response.json();
-  })
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(function (response) {
+            return response.json();
+        })
 
-  .then(function(json) {
-div.textContent= json;
-  })
-
+        .then(function (json) {
+            div.textContent = json;
+        })
 });

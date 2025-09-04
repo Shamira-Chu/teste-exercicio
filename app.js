14d4e113e5bde33e576ec5be4d8e952971cd6c1e
Shamira <shamiraxp10@gmail.com>
2025-09-04 11:06:54 -0300

    remarcando

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let button = document.getElementById('btnUsers');
 button.addEventListener('click', function() {
     //assincrona(Async Chaining)
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
     //console.log('Clicou');
 });

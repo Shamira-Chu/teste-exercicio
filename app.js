34797cadb3b441968d5da23941105958e7c05a20
Shamira <shamiraxp10@gmail.com>
2025-09-11 08:17:24 -0300

    modificando comigo

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let button = document.getElementById('btnUsers');
 let div = document.getElementById('out');
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     //assincrona(Async Chaining)
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
- div.innerText =json.title;
- //div.innerText = JSON.stringify(json);
-  })
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            div.innerText = json.title;
+            //div.innerText = JSON.stringify(json);
+        })
 });

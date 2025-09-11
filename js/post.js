6663d2bac9bf6cba0553b4c624f42a3bd31c4459
Shamira <shamiraxp10@gmail.com>
2025-09-11 09:19:17 -0300

    estruturando class post

--- js/post.js
+++ js/post.js
@@ -1,1 +1,9 @@
-class Post {}
+// Data Model ( Modelo de Dados )
+class Post {
+    constructor(userId, id, title, body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+}
